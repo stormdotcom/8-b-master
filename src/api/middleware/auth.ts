0c7f40398011536a90369b667ebdc5be605b3f96
Ajmal Nasumudeen <ajmaln73@gmail.com>
2024-12-19 01:08:45 +0530

    added fixes

--- src/api/middleware/auth.ts
+++ src/api/middleware/auth.ts
@@ -5,7 +5,7 @@ import { decrypt } from "../../utils/cryptoUtils";
 
 export const validateCustomToken = (req: Request, res: Response, next: NextFunction): void => {
 
-  const token = req.headers.authorization;
+  const token = req.headers.authorization || "";
 
   if (!token) {
      res.status(401).json({ error: "Unauthorized: No token provided" });
